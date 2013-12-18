d543a2e971b75933b95cb149c84711204b3420f9
omardelarosa <thedelarosa@gmail.com>
2013-12-17 20:57:44 -0500

    added heroku production config

--- config/database.rb
+++ config/database.rb
@@ -1,8 +1,22 @@
 # establishes our connection to the DB and other settings
-ActiveRecord::Base.establish_connection(
-    :adapter => "postgresql",
-    :host => "localhost",
-    :username => "omardelarosa",
-    :database => "lambda_feed",
-    :encoding => "utf8"
+configure :development do 
+  ActiveRecord::Base.establish_connection(
+      :adapter => "postgresql",
+      :host => "localhost",
+      :username => "omardelarosa",
+      :database => "lambda_feed",
+      :encoding => "utf8"
+    )
+end
+
+configre :production do 
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_OLIVE_URL'])
+  #configuration info
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
   )
+end
